fcf2360b41e9c1939e7ca0c1705957b60b002413
Gábor Pribék <kapor00@gmail.com>
2025-11-15 09:43:14 +0100

    Initial Commit

---
+++ gyrospline/gyrosplineApp.swift
@@ -0,0 +1,17 @@
+//
+//  gyrosplineApp.swift
+//  gyrospline
+//
+//  Created by Gábor Pribék on 2025. 11. 15..
+//
+
+import SwiftUI
+
+@main
+struct gyrosplineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
